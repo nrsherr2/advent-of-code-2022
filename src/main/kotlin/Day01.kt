9b35f2dba5a645216a91b15f10d95f89a8b909e0
Nicholas Sherrill <nsherrill@messerfinancial.com>
2022-12-01 08:29:18 -0500

    day 1

--- src/main/kotlin/Day01.kt
+++ src/main/kotlin/Day01.kt
@@ -1,9 +1,37 @@
+import kotlin.math.max
+
 class Day01 {
-    fun part1(input: List<String>): Int {
-        TODO()
+    val part1TestExpected = 24000L
+    val part2TestExpected = 45000L
+
+    fun part1(input: List<String>): Long {
+        var curMax = Long.MIN_VALUE
+        var acc: Long = 0
+        input.forEach {
+            if (it.isBlank()) {
+                curMax = max(acc, curMax)
+                acc = 0
+            } else {
+                acc += it.toLong()
+            }
+        }
+        curMax = max(acc, curMax)
+        return curMax
     }
 
-    fun part2(input: List<String>): Int {
-        TODO()
+    fun part2(input: List<String>): Long {
+        val resuts = mutableListOf<Long>()
+        var acc: Long = 0
+        input.forEach {
+            if (it.isBlank()) {
+                resuts.add(acc)
+                acc = 0
+            } else {
+                acc += it.toLong()
+            }
+        }
+        resuts.add(acc)
+        val sorted = resuts.sortedDescending()
+        return sorted.take(3).sum()
     }
 }
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,20 +5,18 @@ fun main(args: Array<String>) {
     val day = Day01()
     val part1Only = false
     val includeTesting = true
-    val part1TestExpected = -1
-    val part2TestExpected = -1
 
     if (includeTesting) {
         val testInput = readInput("Day${dayNum}_Test")
         val part1TestOutput = day.part1(testInput)
-        assertEquals(part1TestExpected, part1TestOutput, 1)
+        assertEquals(day.part1TestExpected, part1TestOutput, 1)
         if (!part1Only) {
             val part2TestOutput = day.part2(testInput)
-            assertEquals(part2TestExpected, part2TestOutput, 2)
+            assertEquals(day.part2TestExpected, part2TestOutput, 2)
         }
     }
 
-    val actualInput = readInput("Day${dayNum}_Test")
+    val actualInput = readInput("Day${dayNum}_Input")
     val timeToExecute = measureTimeMillis {
         val part1Output = day.part1(actualInput)
         val part2Output = if (part1Only) null else day.part2(actualInput)
