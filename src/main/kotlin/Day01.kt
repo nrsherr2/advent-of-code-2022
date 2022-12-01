590d57f3d019e5210d6dff8f38d0640056839aed
Nicholas Sherrill <nsherrill@messerfinancial.com>
2022-12-01 09:59:00 -0500

    multiple ways of part 1

--- src/main/kotlin/Day01.kt
+++ src/main/kotlin/Day01.kt
@@ -1,22 +1,39 @@
 import kotlin.math.max
+import kotlin.random.Random
 
 class Day01 {
     val part1TestExpected = 24000L
     val part2TestExpected = 45000L
 
     fun part1(input: List<String>): Long {
-        var curMax = Long.MIN_VALUE
-        var acc: Long = 0
-        input.forEach {
-            if (it.isBlank()) {
-                curMax = max(acc, curMax)
-                acc = 0
-            } else {
-                acc += it.toLong()
+        /**
+         * 2 ways of doing it, randomly choose
+         */
+        return when (Random.nextInt(0, 3)) {
+            0 -> {
+                var curMax = Long.MIN_VALUE
+                var acc: Long = 0
+                input.forEach {
+                    if (it.isBlank()) {
+                        curMax = max(acc, curMax)
+                        acc = 0
+                    } else {
+                        acc += it.toLong()
+                    }
+                }
+                max(acc, curMax)
+            }
+
+            1 -> {
+                input.joinToString("\n").split("\n\n").maxOf { group -> group.split("\n").sumOf { it.toLong() } }
+            }
+
+            else -> {
+                input.fold(Pair(0L, 0L)) { (max, acc), str ->
+                    if (str.isBlank()) Pair(max(max, acc), 0L) else Pair(max, acc + str.toLong())
+                }.first
             }
         }
-        curMax = max(acc, curMax)
-        return curMax
     }
 
     fun part2(input: List<String>): Long {
