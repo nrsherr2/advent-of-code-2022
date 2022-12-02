21e8d1782a3375a6f0d9ef6fbaf7b9fa26e5720a
Nicholas Sherrill <nsherrill@messerfinancial.com>
2022-12-02 09:46:42 -0500

    comments

--- src/main/kotlin/Day02.kt
+++ src/main/kotlin/Day02.kt
@@ -15,13 +15,17 @@ class Day02 {
 
     fun part2(input: List<String>): Int {
         return input.asSequence().map { line -> line[0] to line[2] }.sumOf { (opponent, outcome) ->
-            //x: lose, y: draw, z: win
+            /**
+             * if you were to lay R, P, S in a row with wraparound, then the losing option is the result
+             * of 2 steps to the right, and the winning option is 1 step to the right
+             */
             val (add, score) = when (outcome) {
                 'X' -> 2 to 0
                 'Y' -> 0 to 3
                 'Z' -> 1 to 6
                 else -> throw IllegalArgumentException()
             }
+            //we're not calculating what you throw, just what you'll get when you throw it
             val player = ((opponent - 'A' + add) % 3) + 1
             player + score
         }
