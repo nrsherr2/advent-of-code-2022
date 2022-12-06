003a3d6b906d539402163cd3fed0d2d18abe8e84
Nicholas Sherrill <nsherrill@messerfinancial.com>
2022-12-06 12:09:03 -0500

    day 6

--- src/main/kotlin/Day06.kt
+++ src/main/kotlin/Day06.kt
@@ -1,11 +1,16 @@
 class Day06 {
-    val part1TestExpected = -1
-    val part2TestExpected = -1
+    val part1TestExpected = 7
+    val part2TestExpected = 19
     fun part1(input: List<String>): Int {
-        TODO()
+        //detect start of packet
+        //start of packet is four characters in a row that are all different
+        //wtf: number character where the window has 4 different letters
+        val realInput = input[0]
+        return realInput.windowed(4, 1).indexOfFirst { it.toCharArray().distinct().size == it.length } + 4
     }
 
     fun part2(input: List<String>): Int {
-        TODO()
+        val realInput = input[0]
+        return realInput.windowed(14, 1).indexOfFirst { it.toCharArray().distinct().size == it.length } + 14
     }
 }
